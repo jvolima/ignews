c7f3959470d35cb33f09719fdabb9ff733188ad8
jvolima <jvolima2004@gmail.com>
2022-02-06 11:57:29 -0300

    ignews

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from "faunadb"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from "../../../services/fauna"
 
 export default NextAuth({
   providers: [
@@ -9,8 +11,45 @@ export default NextAuth({
       authorization: {
         params: {
           scope: "read:user"
-        }
-      }
+        },
+      },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection("users"),
+              {
+                data: {
+                  email: user.email
+                }
+              }
+            ),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+
+      } catch {
+        return false
+      }
+    },
+  }
 })
